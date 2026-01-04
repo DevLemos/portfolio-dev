a7c447d82ca286c28b0ac8e8850f0265791a628e
DevLemos <kauancode@outlook.com>
2026-01-04 13:36:03 -0300

    :sparkles: feat: Iniciando section Hero

--- src/assets/pages/Home/Sections/Hero.jsx
+++ src/assets/pages/Home/Sections/Hero.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import HeroImage from "../../../img/img-main.svg";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <>
-      <section className="home" id="home"></section>
+      <section className="hero-section">
+        <div className="hero-content">
+          <div className="hero-description">
+            <h3>Olá, eu sou</h3>
+            <h1>Kauan Lemos</h1>
+            <h3>Engenheiro de Software</h3>
+            <p>
+              Engenheiro de Software apaixonado por criar soluções digitais
+              eficientes e escaláveis. Transformo ideias em sistemas robustos,
+              combinando tecnologia, desempenho e experiência do usuário.
+            </p>
+          </div>
+          <div className="hero-image">
+            <img src={HeroImage} />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
