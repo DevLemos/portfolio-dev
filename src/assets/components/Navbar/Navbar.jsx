58168830890dca995f1ae0e27df831c8666bc51e
DevLemos <kauancode@outlook.com>
2026-04-03 14:45:39 -0300

    :iphone: Ajustando responsividade

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 import Logo from "../../../../public/favicon.svg";
 import IconMenu from "../../img/menu.svg";
-import CloseMenu from "../../img/xmark-solid-full.svg";
+import CloseMenu from "../../img/window-close-solid.svg";
 import "./Navbar.css";
 
 export default function Navbar() {
   const links = [
     { name: "Home", href: "#home" },
-    { name: "Sobre", href: "#sobremim" },
+    { name: "Sobre", href: "#sobre" },
     { name: "Experiências", href: "#experiencias" },
     { name: "Skills", href: "#skills" },
     { name: "Projetos", href: "#projetos" },
@@ -17,36 +19,124 @@ export default function Navbar() {
   const [open, setOpen] = React.useState(false);
 
   React.useEffect(() => {
-    if (open) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-    return () => {
-      document.body.style.overflow = "";
-    };
+    document.body.style.overflow = open ? "hidden" : "";
+    document.documentElement.style.overflow = open ? "hidden" : "";
   }, [open]);
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+
+    const mudancaTamanho = (e) => {
+      if (e.matches) {
+        setOpen(false);
+      }
+    };
+
+    mediaQuery.addEventListener("change", mudancaTamanho);
+
+    return () => {
+      mediaQuery.removeEventListener("change", mudancaTamanho);
+    };
+  }, []);
 
   return (
     <>
       <header className="navbar">
         <img className="logo" src={Logo} alt="Logo K" />
 
-        <button className="btn-menu" onClick={() => setOpen(!open)}>
+        <button
+          className={`btn-menu ${!open ? "active" : "hidden"}`}
+          onClick={() => setOpen(!open)}
+        >
           <img src={IconMenu} alt="Icone Menu" />
         </button>
 
-        <button
-          className="btn-close"
-          style={{ display: open ? "block" : "none" }}
+        <>
+          <AnimatePresence>
+            {open && (
+              <motion.div
+                className="overlay"
+                onClick={() => setOpen(false)}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+            )}
+          </AnimatePresence>
+
+          <motion.nav
+            id="navbarId"
+            className={`nav-links ${open ? "open" : ""}`}
+            initial={false}
+            animate={{ x: open ? 0 : "0%" }}
+            // exit={{ x: "100%" }}
+            transition={{ duration: 0.3 }}
+          >
+            <button
+              className={`btn-close ${open ? "active" : "hidden"}`}
+              onClick={() => setOpen(false)}
+            >
+              <img src={CloseMenu} alt="Icone de x para fechar o menu" />
+            </button>
+            <ul>
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link to={link.href} onClick={() => setOpen(false)}>
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </motion.nav>
+        </>
+
+        {/* <AnimatePresence>
+          {open && (
+            <>
+              <motion.div
+                className="overlay"
+                onClick={() => setOpen(false)}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              />
+
+              <motion.nav
+                id="navbarId"
+                className={`nav-links ${open ? "open" : ""}`}
+                initial={{ x: "100%" }}
+                animate={{ x: 0 }}
+                exit={{ x: "100%" }}
+                transition={{ duration: 0.3 }}
+              >
+                <button
+                  className={`btn-close ${open ? "active" : "hidden"}`}
+                  onClick={() => setOpen(false)}
+                >
+                  <img src={CloseMenu} alt="Icone de x para fechar o menu" />
+                </button>
+                <ul>
+                  {links.map((link) => (
+                    <li key={link.href}>
+                      <a href={link.href} onClick={() => setOpen(false)}>
+                        {link.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </motion.nav>
+            </>
+          )}
+        </AnimatePresence> */}
+
+        {/* <button
+          className={`btn-close ${open ? "active" : "hidden"}`}
           onClick={() => setOpen(false)}
         >
           <img src={CloseMenu} alt="Icone de x para fechar o menu" />
-        </button>
+        </button> */}
 
-        {/* <span className="decoration-line"></span> */}
-
-        <nav id="navbarId" className={`nav-links ${open ? "open" : ""}`}>
+        {/* <nav id="navbarId" className={`nav-links ${open ? "open" : ""}`}>
           <ul>
             {links.map((link) => (
               <li key={link.href}>
@@ -56,7 +146,7 @@ export default function Navbar() {
               </li>
             ))}
           </ul>
-        </nav>
+        </nav> */}
       </header>
     </>
   );
